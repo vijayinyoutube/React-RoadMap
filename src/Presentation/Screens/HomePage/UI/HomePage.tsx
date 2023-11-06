08e7352d6a1287e00dbf4e506369a9cd6a5bcbfc
vijayinyoutube <vijaythevirtue@gmail.com>
2023-11-06 06:08:24 +0530

    enhancements

--- src/Presentation/Screens/HomePage/UI/HomePage.tsx
+++ src/Presentation/Screens/HomePage/UI/HomePage.tsx
@@ -1,20 +1,27 @@
-import { Button, Steps } from "antd";
+import { Button } from "antd";
 import { useState } from "react";
+import "../CSS/stepper_styles.css";
+import StepperComp from "../Comp/Stepper";
+import { HeightSpacer } from "../../../Components/Spacer";
+
+import { Typography } from "antd";
+import REGAgent from "../Comp/REG_Agent";
+const { Text } = Typography;
 
 const HomePage = () => {
   const description = "This is a description.";
   const steps = [
     {
-      title: "First",
-      content: "First-content",
+      title: "Fill the form",
+      description: "Details needs to be filled",
     },
     {
-      title: "Second",
-      content: "Second-content",
+      title: "Payment Details",
+      description: "Make payment and receive bonus",
     },
     {
-      title: "Last",
-      content: "Last-content",
+      title: "Receive Credits",
+      description: "Get clarification",
     },
   ];
 
@@ -28,18 +35,42 @@ const HomePage = () => {
     setCurrent(current - 1);
   };
 
-
-  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+  const content = [
+    <>
+      <div>Page1</div>
+    </>,
+    <>
+      <div>Page2</div>
+    </>,
+    <>
+      <div>Page3</div>
+    </>,
+  ];
 
   return (
     <div className="flex justify-center items-center bg-sky-500 w-screen h-screen">
-      <div className="bg-white w-1/3  h-96 rounded-lg">
-        <Steps 
-        className="p-kSpace_20"
-        current={current} items={steps} />
-        <Button type="primary" onClick={() => next()} className="bg-btnColor">
-            Next
-          </Button>
+      <div className="bg-[#f6f8fa] xl:w-1/3 lg:w-1/2 md:w-1/2 w-auto m-5 h-auto rounded-xl border-4 border-white">
+        {/* * *  Stepper Component * * */}
+        {StepperComp(current, steps)}
+
+        <div className="px-kSpace_20">
+          {/* * *  Title Text * * */}
+          <Text className="text-kFontNormal flex justify-center font-medium ">
+            Let's calculate your Federal Tax
+          </Text>
+
+          {/* First Component */}
+          {HeightSpacer({ heightClass: "py-kSpace_10" })}
+          {REGAgent()}
+        </div>
+
+        <Button
+          type="primary"
+          onClick={() => next()}
+          className="bg-btnColor m-kSpace_20"
+        >
+          Next
+        </Button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -12,14 +9,20 @@ export default {
         btnColor: "rgb(249 115 22)",
       },
       spacing: {
-        kSpace_20: "20px",
+        kSpace_5: "5px",
         kSpace_10: "10px",
+        kSpace_20: "20px",
       },
     },
     // corePlugins: {
     //   preflight: false,
     // }
+    fontSize: {
+      kFontVerySmall: "12px",
+      kFontSmall: "15px",
+      kFontNormal: "18px",
+      kFontlarge: "20px",
+    },
   },
   plugins: [],
-}
-
+};
